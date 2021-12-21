5fb5f3eb558024b025f4ed062ae6295be7387861
Sumi <ksm2920@gmail.com>
2021-12-21 19:49:59 +0100

    add some styling for the chat room

--- src/components/ChatModal.tsx
+++ src/components/ChatModal.tsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FireClient } from "../FireClient";
 
 const ChatModal = () => {
     const [show, setShow] = useState(true);
+    const [showBtn, setShowBtn] = useState(false);
     const [chatId, setChatId] = useState("");
     const [newMessage, setNewMessage] = useState("");
 
     const createNewChat = () => {
         let random = Math.floor(Math.random() * 100) + 1;
-        const chatNum = "Chat" + random;
+        const chatNum = "Chat " + random;
         FireClient.db.collection("chats").doc(chatNum).set({
             id: chatNum,
             created: new Date()
         })
         setChatId(chatNum);
+    }
+
+    const handleOnChange = (e: any) => {
+        setNewMessage(e.target.value);
     }
 
     const handelOnSubmit = (e: any) => {
@@ -22,28 +27,44 @@ const ChatModal = () => {
         setNewMessage("");
     }
 
-    const handleOnChange = (e: any) => {
-        setNewMessage(e.target.value);
+    const deleteChat = async () => {
+        let docRef = await FireClient.db.collection("chats").doc(chatId).collection("messages");
+        docRef.get().then((querySnapshot) => {
+            Promise.all(querySnapshot.docs.map((d) => d.ref.delete()));
+        })
+        await FireClient.db.collection("chats").doc(chatId).delete();
+        window.location.reload();
     }
 
     return <div className="chat-modal">
-        <button onClick={() => {
-            createNewChat();
-            setShow(false);
-        }}>Support</button>
+        <div className="support-btn" hidden={showBtn}>
+            <button onClick={() => {
+                createNewChat();
+                setShow(false);
+                setShowBtn(true);
+            }}>Support</button>
+        </div>
         <div className="chat-box" hidden={show}>
-            <h1>{chatId}</h1>
-            <form onSubmit={handelOnSubmit} className="message-input">
-                <input
-                    type="text"
-                    value={newMessage}
-                    onChange={handleOnChange}
-                    placeholder="Write a message"
-                />
-                <button type="submit" disabled={!newMessage}>
-                    Send
-                </button>
-            </form>
+            <div className="chat-header">
+                <div>
+                    <button className="leave-btn" onClick={deleteChat}>X</button>
+                </div>
+                <h1>{chatId}</h1>
+            </div>
+            <div className="message-input">
+                <form onSubmit={handelOnSubmit}>
+                    <input
+                        type="text"
+                        value={newMessage}
+                        onChange={handleOnChange}
+                        placeholder="Write a message"
+                    />
+                    {/* <button type="submit" disabled={!newMessage}>
+                        Send
+                    </button> */}
+                </form>
+            </div>
+
         </div>
     </div>
 }
